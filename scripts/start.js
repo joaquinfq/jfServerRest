58d9921c888fec82fec1396426c0196fa2bdefd5
Joaquín Fernández <jf@joaquinfernandez.net>
2018-09-28 10:47:13 +0200

    Actualizado a jfServer@0.2.1

--- scripts/start.js
+++ scripts/start.js
@@ -1,10 +1,8 @@
+const resolve    = require('path').resolve;
 if (!process.argv[3])
 {
-    process.argv[3] = '../www';
+    process.argv[3] = resolve(__dirname, '..', 'www');
 }
-const start                  = require('jf-server/scripts/start');
-start.handlers.jfServerError = require('../src/handler/Error');
-start.register('DELETE', require('../src/handler/Delete'));
-start.register('GET', require('../src/handler/Get'));
-start.register('POST', require('../src/handler/Post'));
-start.register('PATCH', require('../src/handler/Patch'));
+const server                  = require('jf-server/scripts/start');
+server.handlers.jfServerError = require('../src/handler/Error');
+server.registerFromDir(resolve(__dirname, '..', 'src', 'handler'));
